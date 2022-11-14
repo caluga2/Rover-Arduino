80c63e85fb25628877f4525327d3085bb5d388df
caluga2 <carolfreitas50@gmail.com>
2022-11-14 17:28:33 -0300

    bluetooth funcionando

--- Rover-Arduino.ino
+++ Rover-Arduino.ino
@@ -1,63 +1,108 @@
+#include<SoftwareSerial.h>
+SoftwareSerial BT(4,2);
+
 //ubicación de los pines
-#define EN1 6 //velocidad motor A
+#define ENA 6 //velocidad motor A
 #define IN1 9
 #define IN2 10
 
-#define EN2 5 //velocidad motor A
+#define ENB 5 //velocidad motor A
 #define IN3 11
 #define IN4 12
 
 void setup() {
-  // codigo base
-  pinMode(EN1, OUTPUT);
+
+ Serial.begin(9600);
+ BT.begin(9600);
+
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
-  pinMode(EN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
-  digitalWrite(IN1, HIGH); //adelante por 1.5s
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  analogWrite(EN1, 255);
-  analogWrite(EN2, 255);
-  delay(1500);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
 
-  digitalWrite(IN1, LOW); //parar por 0.5s
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-  analogWrite(EN1, 0);
-  analogWrite(EN2, 0);
-  delay(500);
+  //digitalWrite(IN1, LOW);
+  //digitalWrite(IN2, LOW);
+  //digitalWrite(IN3, LOW);
+  //digitalWrite(IN4, LOW);
 
-  digitalWrite(IN1, HIGH); //girar por 1s
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(EN1, 255);
-  analogWrite(EN2, 255);
-  delay(1000);
+  //analogWrite(EN1,63);
+  //analogWrite(EN2,63);
 
-  digitalWrite(IN1, LOW); //girar para el otro lado por 1s
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  analogWrite(EN1, 255);
-  analogWrite(EN2, 255);
-  delay(1000);
-
-  digitalWrite(IN1, LOW); //parar por 0.5s
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-  analogWrite(EN1, 0);
-  analogWrite(EN2, 0);
-  delay(500);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (BT.available()){
+    char dato=BT.read();
+    Serial.println(dato);
+    switch(dato){
+      case 'a':
+        Adelante();
+        break;
+      case 'r':
+        Atras();
+        break;
+      case 'd':
+        Derecha();
+        break;
+      case 'i':
+        Izquierda();
+        break;
+      case 'p':
+        Parar();
+        break;
+    }    
+  }
+}
+void Atras (){
+ //Dirección motor A
+ digitalWrite (IN1, LOW);
+ digitalWrite (IN2, HIGH);
+ analogWrite (ENA, 255); //Velocidad motor A
+ //Dirección motor B
+ digitalWrite (IN3, HIGH);
+ digitalWrite (IN4, LOW);
+ analogWrite (ENB, 255); //Velocidad motor B
+}
+void Izquierda (){
+ //Dirección motor A
+ //digitalWrite (IN1, HIGH);
+ //digitalWrite (IN2, LOW);
+ analogWrite (ENA, 170); //Velocidad motor A
+ //Dirección motor B
+ //digitalWrite (IN3, HIGH);
+ //digitalWrite (IN4, LOW);
+ analogWrite (ENB, 255); //Velocidad motor A
+}
+void Derecha (){
+ //Dirección motor A
+ //digitalWrite (IN1, LOW);
+ //digitalWrite (IN2, HIGH);
+ analogWrite (ENA, 255); //Velocidad motor A
+ //Dirección motor B
+ //digitalWrite (IN3, LOW);
+ //digitalWrite (IN4, HIGH);
+ analogWrite (ENB, 170); //Velocidad motor A
+}
+void Adelante (){
+ //Dirección motor A
+ digitalWrite (IN1, HIGH);
+ digitalWrite (IN2, LOW);
+ analogWrite (ENA, 255); //Velocidad motor A
+ //Dirección motor B
+ digitalWrite (IN3, LOW);
+ digitalWrite (IN4, HIGH);
+ analogWrite (ENB, 255); //Velocidad motor B
+}
+void Parar (){
+ //Dirección motor A
+ digitalWrite (IN1, LOW);
+ digitalWrite (IN2, LOW);
+ analogWrite (ENA, 0); //Velocidad motor A
+ //Dirección motor B
+ digitalWrite (IN3, LOW);
+ digitalWrite (IN4, LOW);
+ analogWrite (ENB, 0); //Velocidad motor A
 }
